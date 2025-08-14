2fac6c4153d368a2dc738f14fd917d974a52f1ad
Yanick Senn <ys+github@yanicksenn.com>
2025-08-14 17:06:11 +0200

    change binary name

--- Formula/gtasks-cli.rb
+++ Formula/gtasks-cli.rb
@@ -7,7 +7,7 @@ class GtasksCli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+    system "go", "build", *std_go_args(ldflags: "-s -w"), ".", "-o", "gtasks"
   end
 
   test do
